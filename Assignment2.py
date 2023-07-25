26436b26304091cf27ddf1f483adb9627e7681eb
gghugare <83158085+gghugare@users.noreply.github.com>
2023-07-25 15:48:39 +0000

    Garde System Done

---
+++ Assignment2.py
@@ -0,0 +1,15 @@
+# Grade System
+
+avg = int(input('Enter the number'))
+
+if avg>=80 and avg<=100:
+    print("Your Grade is A")
+elif avg>=60 and avg<79:
+    print("Your Grade is B")
+elif avg>=40 and avg<59:
+    print("Your Grade is C")
+elif avg>=30 and avg<39:
+    print("Your Grade is D")
+else: 
+    avg<29
+    print("Your Grade is F")
