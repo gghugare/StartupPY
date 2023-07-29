67e1d8a5d16855aff3d6dd4eea1e0a900f1b9a4e
gghugare <83158085+gghugare@users.noreply.github.com>
2023-07-29 15:43:49 +0000

    Age Cal

---
+++ Assignment3.py
@@ -0,0 +1,19 @@
+Year1=int(input("My birth year is : "))
+Month1=int(input("My birth month is : "))
+Day1=int(input("My birth day is : "))
+
+CurrentYear2=int(input("Enter your Current Year : "))
+CurrentMonth2=int(input("Enter your Current Month : "))
+CurrentDay2=int(input("Enter your Current Day : "))
+
+print("My birth year is  : ",Year1)
+print("My birth month is : ",Month1)
+print("My birth day is : ",Day1)
+
+print("Current year is : ",CurrentYear2)
+print("Current month : ",CurrentMonth2)
+print("Current day is : ",CurrentDay2)
+
+print(CurrentYear2-Year1, 'year')
+print(CurrentMonth2-Month1, 'month')
+print(CurrentDay2-Day1, 'day')
