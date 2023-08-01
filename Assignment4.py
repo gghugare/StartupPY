218550be05bbece49d3ef5acd8c6df2fcf665f29
gghugare <83158085+gghugare@users.noreply.github.com>
2023-08-01 15:46:43 +0000

    sum of natural num

---
+++ Assignment4.py
@@ -0,0 +1,23 @@
+
+# Example : 
+# Enter Start Number : 1
+# Enter End Number : 10
+
+# -----------------
+# total sum of 1 to 10 = 55
+# ----------------- 
+
+a=0
+for num in range(1,11):
+    a=num+a
+    print(a,end=' ')
+
+
+
+
+
+
+    a=2
+for i in range(1,11):
+    sum=i*a
+    print(a)
