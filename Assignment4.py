e825674fe97321484ca0e7a127e69be3c1245d39
gghugare <83158085+gghugare@users.noreply.github.com>
2023-08-03 15:35:07 +0000

    alphabets counts

--- Assignment4.py
+++ Assignment4.py
@@ -7,17 +7,78 @@
 # total sum of 1 to 10 = 55
 # ----------------- 
 
-a=0
+
+
+a = 0
 for num in range(1,11):
     a=num+a
     print(a,end=' ')
 
 
 
+''' Write a Program to Generate Multiplication Table ?
+
+
+Enter print Table Number : 2
+
+2 x 1  = 2
+2 x 2  = 4
+.
+.
+2 x 10 = 20 '''
 
 
 
-    a=2
+
+
+
+sum = int(input(" Enter a number "))
 for i in range(1,11):
-    sum=i*a
-    print(a)
+    result = sum*i
+    print(sum," * ",i," = ",result)
+
+
+
+
+'''  Write a Program to Display count of Alphabets in to String
+
+   Enter Word : p4n.in
+
+   total = count of Alphabets : 6 '''
+
+
+Num = 0
+name = str(input(" Enter a word "))
+for g in name :
+    Num = Num+1
+    print("count of Alphabets : ", Num)
+
+
+
+'''Write a Program to Display count of Alphabets in to String
+
+   Enter Word : p4n.in
+
+   total = count of Alphabets : 6'''
+
+
+
+mum = 0
+same = str(input(" Enter a num "))
+for n in same :
+    mum = mum+1
+    print("count of Numbers : ", mum)
+
+
+
+
+    '''Write a Program to Reverse a Number
+
+   Enter Number : 4569
+   Reverse Number : 9654 '''
+    
+
+
+
+
+ 
