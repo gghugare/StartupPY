b9e3f5dee4f22bbb37eecef7b652d182b1eabc99
gghugare <83158085+gghugare@users.noreply.github.com>
2023-08-19 15:30:10 +0000

    little update

---
+++ Assignment6.py
@@ -0,0 +1,5 @@
+list_val = []  
+num_list = int(input("Enter number of elements in list: ")) 
+for i in range(num_list):  
+    element = int(input("Enter the elements: "))  
+    list_val.append(element)   
